cb4bd1000fbde0e2b85acb895d9459878602d644
Matt Van <mvan@riotgames.com>
2016-06-22 17:22:43 -0700

    changes for streaming hls

--- build.sh
+++ build.sh
@@ -21,7 +21,6 @@ docker rmi ${DOCKER_BUILD_IMAGE}
 
 docker build -t ${DOCKER_RUN_IMAGE} .
 
-docker images 
-docker run -d --name ${DOCKER_RUN_IMAGE} -p 1935:1935 -t ${DOCKER_RUN_IMAGE}
+docker images
+docker run -d --name ${DOCKER_RUN_IMAGE} -p 1935:1935 -p 8080:8080 -t ${DOCKER_RUN_IMAGE}
 docker ps -a
-
